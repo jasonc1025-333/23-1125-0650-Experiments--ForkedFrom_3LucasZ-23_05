03dddf622f1eaca956e647554565ed7873e06b63
3LucasZ <lucas.zheng@warriorlife.net>
2021-10-11 00:26:17 -0700

    Fine tuning so it works with RPI instead of webcam

--- UnityBackend/UDPImageStream.py
+++ UnityBackend/UDPImageStream.py
@@ -1,21 +1,22 @@
 import socket
 import cv2
 import time
-import base64
 
 #config
-MY_ADDR = ("192.168.1.144", 5000)
+MY_ADDR = ("192.168.1.188", 5000)
 OTHER_ADDR = ("192.168.1.144", 6000)
 #MY_ADDR = ("", 5000)
 #OTHER_ADDR = ("192.168.1.139", 5000)
 
 #setup
 camera = cv2.VideoCapture(0)
-camera.set(3, 120)
-camera.set(4, 80)
+camera.set(3, 128)
+camera.set(4, 128)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-numSegments = 10
+
 #run
+print("wait for warm up...")
+time.sleep(3)
 print("sending started")
 currentTime = time.time()
 while True:
@@ -23,5 +24,6 @@ while True:
     currentTime = time.time()
     print("frame time:", currentTime - prevTime)
     retval, frame = camera.read()
+    frame = cv2.flip(frame, -1)
     retval, jpg = cv2.imencode('.jpg', frame)
     sock.sendto(jpg, OTHER_ADDR)
