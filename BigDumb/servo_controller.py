9c1371a939772ad9c3d8782fec6eea613907ec26
3LucasZ <lucas.j.zheng@gmail.com>
2021-10-11 20:11:21 -0700

    Update servo_controller.py

--- BigDumb/servo_controller.py
+++ BigDumb/servo_controller.py
@@ -239,11 +239,14 @@ def callback_servoPWR_enable(msg):
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 
-callback_servoPWR_enable(msg(True))
-callback_servo0_enable(msg(True))
-callback_servo0_angle(msg(50))
-
-#GPIO.cleanup()
 class msg:
 	def __init__(self, data):
 		self.data = data
+
+msg1 = msg(True)
+msg2 = msg(50)
+callback_servoPWR_enable(msg1)
+callback_servo0_enable(msg1)
+callback_servo0_angle(msg2)
+
+#GPIO.cleanup()
