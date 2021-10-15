654d200a97a72002883eaea0bd81cbdcf7fd976e
3LucasZ <lucas.j.zheng@gmail.com>
2021-10-14 23:06:07 -0700

    Update receive_servo.py

--- VR/Servos/receive_servo.py
+++ VR/Servos/receive_servo.py
@@ -1,4 +1,4 @@
-ON_PI = False
+ON_PI = True
 
 import socket
 if ON_PI:
@@ -18,7 +18,7 @@ if ON_PI:
 while True:
     data, addr = sock.recvfrom(4)
     #uncomment for verbose
-    print("(", data[0], ",", data[1], ")")
+    #print("(", data[0], ",", data[1], ")")
     if ON_PI:
         pan_angle = sc.msg(data[0])
         tilt_angle = sc.msg(data[1])
