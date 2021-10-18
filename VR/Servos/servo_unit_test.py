76d35524e2fb00c1b79dc6d821343c57639ad20c
3LucasZ <lucas.j.zheng@gmail.com>
2021-10-18 16:41:15 -0700

    Update servo_unit_test.py

--- VR/Servos/servo_unit_test.py
+++ VR/Servos/servo_unit_test.py
@@ -1,7 +1,24 @@
+from flask import Flask, url_for
 import servo_controller as sc
 
-msg1 = sc.msg(True)
-msg2 = sc.msg(50)
-sc.callback_servoPWR_enable(msg1)
-sc.callback_servo0_enable(msg1)
-sc.callback_servo0_angle(msg2)
+app = Flask(__name__)
+
+trueMsg = sc.msg(True)
+falseMsg = sc.msg(False)
+sc.callback_servoPWR_enable(trueMsg)
+
+@app.route("/")
+def main():
+    return "Please enter something like: /0/40"
+
+@app.route('/<servoId>/<servoAngle>')
+def profile(servoId, servoAngle):
+    if servoId == 0:
+		sc.callback_servo0_enable(trueMsg)
+		angle = sc.msg(servoAngle)
+		sc.callback_servo0_angle(angle)
+	elif servoId == 1:
+		sc.callback_servo1_enable(trueMsg)
+		angle = sc.msg(servoAngle)
+		sc.callback_servo1_angle(angle)
+	return url_for('main')
