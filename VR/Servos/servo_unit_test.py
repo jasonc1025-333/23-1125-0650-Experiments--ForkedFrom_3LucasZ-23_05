c5a77760c7efbb893be2dd4b080d91981d88adc2
3LucasZ <lucas.j.zheng@gmail.com>
2021-10-22 21:31:45 -0700

    tuning

--- VR/Servos/servo_unit_test.py
+++ VR/Servos/servo_unit_test.py
@@ -36,3 +36,6 @@ def board_off():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+#servos are at "Home position" at (0: 58, 1:130)
+#servos range at (0: [0, 116], 1: [110, 180])
