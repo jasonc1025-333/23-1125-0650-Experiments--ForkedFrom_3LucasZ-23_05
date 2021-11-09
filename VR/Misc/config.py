85a31987a4683d0117ec0f16d2811e110a2e1a0e
3LucasZ <lucas.j.zheng@gmail.com>
2021-11-08 18:30:26 -0800

    Update config.py

--- VR/Misc/config.py
+++ VR/Misc/config.py
@@ -1,11 +1,11 @@
 #mac on dubhub
 #OPERATOR_IP = "192.168.1.144"
 #mac on raspi-ap
-OPERATOR_IP = "192.168.50.149"
+#OPERATOR_IP = "192.168.50.149"
 #oculus on dubhub
 #OPERATOR_IP = "192.168.1.178"
 #oculus on raspi-ap
-#OPERATOR_IP = "192.168.50.140"
+OPERATOR_IP = "192.168.50.140"
 OPERATOR_CAMERA_PORT = 5001
 
 BOT_IP = ""
