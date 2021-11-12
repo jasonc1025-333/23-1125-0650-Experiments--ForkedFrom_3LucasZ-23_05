3dfabbf07c993d169b96bf35ebf18de880a8cf41
3LucasZ <lucas.j.zheng@gmail.com>
2021-11-11 18:57:09 -0800

    Update audioReceiver.py

--- EC/AudioStreaming/audioReceiver.py
+++ EC/AudioStreaming/audioReceiver.py
@@ -1,5 +1,6 @@
 import socket
 import pyaudio
+import threading
 
 CHUNK = 4096
 FORMAT = pyaudio.paInt8
@@ -14,15 +15,32 @@ sock.bind(MY_ADDR)
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT, channels=1, rate=RATE, output=True)
 
-print("audio player started")
-data, addr = sock.recvfrom(CHUNK)
-while data != '':
-    print("Received.")
-    stream.write(data)
-    data, addr = sock.recvfrom(CHUNK)
-    data, addr = sock.recvfrom(CHUNK)
-    data, addr = sock.recvfrom(CHUNK)
+data = 'empty'
+def receive_thread():
+    global data
+    while data!= '':
+        print("Received.")
+        data, addr = sock.recvfrom(CHUNK)
 
+def play_thread():
+    global data
+    print("audio player started")
+    while data == 'empty':
+        pass
+    while data != '':
+        stream.write(data)
+
+if __name__ == "__main__":
+    # creating thread
+    t1 = threading.Thread(target=receive_thread)
+    t2 = threading.Thread(target=play_thread)
+  
+    # starting thread 1
+    t1.start()
+    # starting thread 2
+    t2.start()
+
+#cleanup
 stream.stop_stream()
 stream.close()
 p.terminate()
